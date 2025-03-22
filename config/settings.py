8f3c3cf2df96322772c73d46d4e513d3266bcb71
Jerad Wolfrum <jerad.wolfrum@gmail.com>
2025-03-22 14:45:10 -0500

    Change myapp folder to be more desicriptive "core"

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'myapp', 'templates')],  # Ensure Django knows where to look for templates
+        'DIRS': [os.path.join(BASE_DIR, 'core', 'templates')],  # Ensure Django knows where to look for templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
